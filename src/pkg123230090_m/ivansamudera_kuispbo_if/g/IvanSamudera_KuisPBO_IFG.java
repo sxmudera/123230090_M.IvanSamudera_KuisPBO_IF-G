617967065db4645cf364cb1437504317662370e2
sxmudera <ivansamudera22@gmail.com>
2025-03-15 15:25:54 +0700

    first commit

---
+++ src/pkg123230090_m/ivansamudera_kuispbo_if/g/IvanSamudera_KuisPBO_IFG.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg123230090_m.ivansamudera_kuispbo_if.g;
+
+/**
+ *
+ * @author ivans
+ */
+public class IvanSamudera_KuisPBO_IFG {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
